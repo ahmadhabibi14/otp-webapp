4f0b1db7b29d64800a151d3c854791ee13d59f05
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2024-03-18 18:20:10 +0800

    connect to redis

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module otp-webapp
 
 go 1.22.1
+
+require github.com/go-redis/redis v6.15.9+incompatible // indirect
--- main.go
+++ main.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"log"
+
+	"github.com/go-redis/redis"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "",
+		DB: 0,
+	})
+
+	_, err := client.Ping().Result()
+	if err != nil {
+		log.Println("failed to connect redis:", err)
+		return
+	} else {
+		log.Println("connected to redis")
+	}
+
+	// Set key value pair
+	err = client.Set("name", "Habibi", 0).Err()
+	if err != nil {
+		log.Println("failed to set new value:", err)
+	}
+	
+	// Retrieve a value for a given key
+	name, err := client.Get("name").Result()
+	if err != nil {
+		log.Println("key is empty:", err)
+	}
+
+	// It must be empty
+	age, err := client.Get("age").Result()
+	if err != nil {
+		log.Println("key is empty:", err)
+	}
+
+	log.Println("name \t:", name)
+	log.Println("age \t:", age)
+}
